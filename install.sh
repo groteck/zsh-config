5b0ad53789f2c82a5b9ac57aecc43205289c84de
Juan Fraire <groteck@gmail.com>
2015-07-07 11:09:09 +0200

    change default user shell

---
+++ install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true && \
+rm ~/.zshrc && \
+curl -o ~/.zshrc https://raw.githubusercontent.com/groteck/zsh-config/master/zshrc
